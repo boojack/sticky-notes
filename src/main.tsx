64266b45e117c0217e4f7ebd79d1c53fe4b4c449
email <imrealleonardo@gmail.com>
2022-04-04 10:32:17 +0800

    feat: upgrade react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 import "./index.less";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container as HTMLElement);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
